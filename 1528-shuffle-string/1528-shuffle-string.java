92c49f2d739a7328695c322ab592f8451e246a90
Aaditya Telange <101004050+Aaditya2704@users.noreply.github.com>
2023-04-06 20:32:57 +0530

    Time: 8 ms (15.47%), Space: 43.3 MB (11.84%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,16 +1,13 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        HashMap<Integer,Character>mp=new HashMap<Integer,Character>();
+       TreeMap<Integer,Character>tree=new TreeMap<Integer,Character>();
         for(int i=0;i<indices.length;i++){
-            mp.put(indices[i],s.charAt(i));
+            tree.put(indices[i],s.charAt(i));
         }
-        ArrayList<Integer> sortByKey=new ArrayList<Integer>(mp.keySet());
-        Collections.sort(sortByKey);
-        
-        StringBuffer sb = new StringBuffer();
-        for(Map.Entry m:mp.entrySet())
-            sb.append(m.getValue());
-      String str = sb.toString();
+        String str ="";
+        for(Map.Entry m:tree.entrySet()){
+            str+=m.getValue();
+        }
         return str;
     }
 }
