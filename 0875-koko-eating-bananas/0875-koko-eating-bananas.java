9bdb1a89584e8eef590af004faeb1da33d800228
Aaditya Telange <101004050+Aaditya2704@users.noreply.github.com>
2023-08-03 15:58:06 +0530

    Time: 28 ms (59.82%), Space: 44 MB (93.74%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,28 +1,18 @@
 class Solution {
-    
-    public static int sumOfD(int []piles,int div){
-        int n=piles.length;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=Math.ceil((double)piles[i]/(double)div);
-        }
-        return sum;
-    }
     public int minEatingSpeed(int[] piles, int h) {
-         int n=piles.length;
-        if(n>h)return -1;
-        int max=Integer.MIN_VALUE;
-        
-        for(int i=0;i<n;i++){
-            max=Math.max(max,piles[i]);
+        int low = 1, high = Integer.MIN_VALUE;
+        for (int i : piles) if (i > high) high = i;
+        int result = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2, hours = 0;
+            for (int i : piles) {
+                hours += Math.ceil((double) i / (double) mid);
+            }
+            if (hours <= h) {
+                result = Math.min(result, mid);
+                high = mid - 1;
+            } else low = mid + 1;
         }
-        int low=1,high=max;
-        
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(sumOfD(piles,mid)<=h)high=mid-1;
-            else low=mid+1;
-        }
-        return low;
+        return result;
     }
 }
